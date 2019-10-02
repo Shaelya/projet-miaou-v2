8fa69728e35408641c325490adef1d263d902b5e
Shaelya <chadenatm@gmail.com>
2019-10-02 12:34:48 +0200

    Navbar

--- react/src/components/App/index.js
+++ react/src/components/App/index.js
@@ -19,12 +19,20 @@ import './app.sass';
 
 const App = ({location, handleClick}) => (
   <div className="App">
-    <nav className="navbar navbar-light bg-light">
-      <a className="navbar-brand" href="#">
+    <nav className="navbar navbar-dark">
+      <i className="fa fa-user-times"></i><p className="ml-2 text-white">non connecté</p>
+      <a className="navbar-brand mx-auto" href="#">
         <img src="/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" alt=""/>
-        MIA'Où
+        <h1 className="h1">MIA'Où</h1>
       </a>
+      <form className="form-inline">
+        <button className="btn btn-info mr-2" type="button">Se connecter</button>
+        <button className="btn btn-info" type="button">S'inscrire</button>
+      </form>
     </nav>
+      <p className="subtitle text-white mb-4">Le site des animaux perdus</p>
+    
+
     <Map location={location} />
     <ButtonGeoloc handleClick={handleClick} />
   </div>
