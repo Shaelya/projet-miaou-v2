64f5846f6402bfd83c8fe003bf151a8beca4a80f
Shaelya <chadenatm@gmail.com>
2019-10-09 15:11:20 +0200

    Base

---
+++ react/src/components/AlertView/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+const AlertView = ({data}) => {
+  let alertData = data.location.state.alertData;
+  console.log(alertData);
+
+  return (
+    <div className="alertview-page">
+    <h1 className="h1">{alertData.name} ref : {alertData.id} </h1>
+    <table className="table">
+      <tbody>
+        <tr>
+          <th>Nom</th>
+            <td>{alertData.name}</td>
+        </tr>
+        <tr>
+          <th>Type</th>
+            <td>{alertData.type}</td>
+        </tr>
+        <tr>
+          <th>Lieu</th>
+            <td></td>
+        </tr>
+        <tr>
+          <th>Sexe</th>
+            <td>{alertData.sex}</td>
+        </tr>
+        <tr>
+          <th>Description</th>
+            <td>{alertData.description}</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <h2 className="h2">Commentaires</h2>
+    <table className="table">
+      <tbody>
+        {alertData.comments.map((comment) => (
+          <tr key={comment.id}>
+          <th>{comment.user.firstName}</th>
+            <td>date</td>
+            <td>{comment.text}</td>
+        </tr>
+        ))}
+      </tbody>
+    </table>
+    
+    <button>Infos proprietaire</button>
+    <button>Poster un commentaire</button>
+
+    {alertData.pictures.map((picture) => (
+      <div key={picture.id}><img src={picture.image} /></div>
+    ))}
+
+
+  </div>
+
+  )
+}
+
+
+export default AlertView;
