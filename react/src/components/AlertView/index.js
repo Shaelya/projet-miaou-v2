8dbc4d296835a26e91bada2175464914385529cf
Shaelya <chadenatm@gmail.com>
2019-10-10 19:13:42 +0200

    Fonctionnalité infos propriétaire ok

--- react/src/components/AlertView/index.js
+++ react/src/components/AlertView/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
+import './alertview.sass';
 
 
 class AlertView extends React.Component {
@@ -15,7 +16,7 @@ class AlertView extends React.Component {
       this.setState({visibility: true})
     } else {
       MySwal.fire({
-        text: 'Le propriétaire a choisi de ne pas divulguer ses informations',
+        text: 'Le propriétaire a choisi de ne pas divulguer ses informations, si vous souhaitez le contacter vous pouvez lui laisser un commentaire',
         type: "error",
         confirmButtonText: 'Ok'
       })
@@ -31,114 +32,109 @@ class AlertView extends React.Component {
 
     if(this.state.visibility){
       return(
-        <div className="alertview-page">
+        <div>
         <h1 className="h1">{alertData.name} ref : {alertData.id} </h1>
-        <table className="table">
-          <tbody>
-            <tr>
-              <th>Nom</th>
-                <td>{alertData.name}</td>
-            </tr>
-            <tr>
-              <th>Type</th>
-                <td>{alertData.type}</td>
-            </tr>
-            <tr>
-              <th>Lieu</th>
-                <td>{alertData.lieu}</td>
-            </tr>
-            <tr>
-              <th>Sexe</th>
-                <td>{alertData.sex}</td>
-            </tr>
-            <tr>
-              <th>Description</th>
-                <td>{alertData.description}</td>
-            </tr>
-          </tbody>
-        </table>
+              <table className="table">
+                <tbody>
+                  <tr>
+                    <th>Nom</th>
+                      <td>{alertData.name}</td>
+                  </tr>
+                  <tr>
+                    <th>Type</th>
+                      <td>{alertData.type}</td>
+                  </tr>
+                  <tr>
+                    <th>Lieu</th>
+                      <td>{alertData.lieu}</td>
+                  </tr>
+                  <tr>
+                    <th>Sexe</th>
+                      <td>{alertData.sex}</td>
+                  </tr>
+                  <tr>
+                    <th>Description</th>
+                      <td>{alertData.description}</td>
+                  </tr>
+                </tbody>
+              </table>
 
-        <h2 className="h2">Commentaires</h2>
-        <table className="table">
-          <tbody>
-            {alertData.comments.map((comment) => (
-              <tr key={comment.id}>
-              <th>{comment.user.firstName}</th>
-                <td>{comment.createdAtJson}</td>
-                <td>{comment.text}</td>
-            </tr>
-            ))}
-          </tbody>
-        </table>
-        
-        <button onClick={this.handleClickInfo}>Infos proprietaire</button>
-        <button>Poster un commentaire</button>
-        <div className="alert alert-warning alert-dismissible fade show" role="alert">
-          <div>Prénom : {alertData.user.firstName}</div>
-          <div>Nom : {alertData.user.lastName}</div>
-          <div>Téléphone : {alertData.user.phone}</div>
-          <div>E-mail : {alertData.user.email}</div>
-          <button onClick={this.handleCloseInfo} type="button" className="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        
-        <img src={alertData.picture} />
+              <h2 className="h2">Commentaires</h2>
+              <table className="table">
+                <tbody>
+                  {alertData.comments.map((comment) => (
+                    <tr key={comment.id}>
+                    <th>{comment.user.firstName}</th>
+                      <td>{comment.createdAtJson}</td>
+                      <td>{comment.text}</td>
+                  </tr>
+                  ))}
+                </tbody>
+              </table>
+              
+              <button onClick={this.handleClickInfo}>Infos proprietaire</button>
+              <button>Poster un commentaire</button>
+              <div className="alert alert-warning alert-dismissible fade show" role="alert">
+                <div>Prénom : {alertData.user.firstName}</div>
+                <div>Nom : {alertData.user.lastName}</div>
+                <div>Téléphone : {alertData.user.phone}</div>
+                <div>E-mail : {alertData.user.email}</div>
+                <button onClick={this.handleCloseInfo} type="button" className="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
 
 
+                <img className="alerte-image" src={alertData.picture} />
+            </div>
 
-      </div>
       )
     } else {
       return(
-        <div className="alertview-page">
+        <div>
         <h1 className="h1">{alertData.name} ref : {alertData.id} </h1>
-        <table className="table">
-          <tbody>
-            <tr>
-              <th>Nom</th>
-                <td>{alertData.name}</td>
-            </tr>
-            <tr>
-              <th>Type</th>
-                <td>{alertData.type}</td>
-            </tr>
-            <tr>
-              <th>Lieu</th>
-                <td>{alertData.lieu}</td>
-            </tr>
-            <tr>
-              <th>Sexe</th>
-                <td>{alertData.sex}</td>
-            </tr>
-            <tr>
-              <th>Description</th>
-                <td>{alertData.description}</td>
-            </tr>
-          </tbody>
-        </table>
+              <table className="table">
+                <tbody>
+                  <tr>
+                    <th>Nom</th>
+                      <td>{alertData.name}</td>
+                  </tr>
+                  <tr>
+                    <th>Type</th>
+                      <td>{alertData.type}</td>
+                  </tr>
+                  <tr>
+                    <th>Lieu</th>
+                      <td>{alertData.lieu}</td>
+                  </tr>
+                  <tr>
+                    <th>Sexe</th>
+                      <td>{alertData.sex}</td>
+                  </tr>
+                  <tr>
+                    <th>Description</th>
+                      <td>{alertData.description}</td>
+                  </tr>
+                </tbody>
+              </table>
 
-        <h2 className="h2">Commentaires</h2>
-        <table className="table">
-          <tbody>
-            {alertData.comments.map((comment) => (
-              <tr key={comment.id}>
-              <th>{comment.user.firstName}</th>
-                <td>{comment.createdAtJson}</td>
-                <td>{comment.text}</td>
-            </tr>
-            ))}
-          </tbody>
-        </table>
-        
-        <button onClick={this.handleClickInfo}>Infos proprietaire</button>
-        <button>Poster un commentaire</button>
-        
-        <img src={alertData.picture} />
-
-
-
-      </div>
+              <h2 className="h2">Commentaires</h2>
+              <table className="table">
+                <tbody>
+                  {alertData.comments.map((comment) => (
+                    <tr key={comment.id}>
+                    <th>{comment.user.firstName}</th>
+                      <td>{comment.createdAtJson}</td>
+                      <td>{comment.text}</td>
+                  </tr>
+                  ))}
+                </tbody>
+              </table>
+              
+              <button onClick={this.handleClickInfo}>Infos proprietaire</button>
+              <button>Poster un commentaire</button>
+                <img className="alerte-image" src={alertData.picture} />
+            </div>
       )
     }
   }
