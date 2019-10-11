ee04f88d52bc966b89d1e4f3b4be0925b7586383
Shaelya <chadenatm@gmail.com>
2019-10-11 17:14:08 +0200

    liens consulter

--- react/src/components/Profil/index.js
+++ react/src/components/Profil/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import'./profil.sass';
 
 const Profil = ({userData, data}) => {
 
-  console.log(data);
   if(userData.userConnected){
     let user = "";
     let comments = "";
@@ -45,25 +45,38 @@ const Profil = ({userData, data}) => {
         <h2 className="h2 mt-5 text-center mb-4">Mes commentaires</h2>
         <table className="table">
           <tbody>
-            {comments.map((comment) => (
+            {comments.map((comment) => {
+              let commentAdvert = data.filter((advert) => comment.advert.id == advert.id);
+              return(
               <tr key={comment.id}>
               <th>{comment.createdAtJson}</th>
                 <td>{comment.advert.name} Ref : {comment.advert.id}</td>
-                <td>consulter</td>
+                <td><Link to={ {
+              pathname: '/fiche-alerte-vue',
+              state: { alertData: commentAdvert[0]  }
+            } }> consulter</Link></td>
             </tr>
-            ))}
+            )
+            })
+          }
           </tbody>
         </table>
         <h2 className="h2 mt-5 text-center mb-4">Mes annonces</h2>
         <table className="table">
           <tbody>
-            {advertData.map((advert) => (
+            {advertData.map((advert) => {
+              console.log(advert);
+              return(
               <tr key={advert.id}>
               <th>{advert.createdAtJson}</th>
                 <td>{advert.name} Ref : {advert.id}</td>
-                <td>consulter</td>
+                <td><Link to={ {
+              pathname: '/fiche-alerte-vue',
+              state: { alertData: advert  }
+            } }> consulter</Link></td>
             </tr>
-            ))}
+            )
+            })}
           </tbody>
         </table>
       </div>
